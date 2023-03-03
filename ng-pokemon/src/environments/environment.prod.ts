60cbb0af421a389b016312c598f8a554d15b1827
andreas-nn-experis <125509027+andreas-nn-experis@users.noreply.github.com>
2023-03-03 17:40:25 +0100

    Moved all files into root folder of GitLab project

--- ng-pokemon/src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-    production: true,
-    
-    apiUsers: "https://assignment-5-slt-production.up.railway.app/trainers",
-    apiKey: "mAqZeJEcikyZ9UP8umoxjg==",
-
-    apiPokemon: "https://pokeapi.co/api/v2/pokemon",
-    apiPokemonSprites: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon"
-};
