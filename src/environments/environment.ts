ada91a95805a55e5dadc6e3c10c61404f0c6b381
andreas-nn-experis <125509027+andreas-nn-experis@users.noreply.github.com>
2023-03-03 17:45:38 +0100

    added environment variables

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    apiUsers: "https://assignment-5-slt-production.up.railway.app/trainers",
+    apiKey: "mAqZeJEcikyZ9UP8umoxjg==",
+
+    apiPokemon: "https://pokeapi.co/api/v2/pokemon",
+    apiPokemonSprites: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon"
+};
